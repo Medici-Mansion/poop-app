76588cf8c1bf0f17a0cb2a225a46b7faff5e4a07
LeeBonHoon <80507816+LeeBonHoon1@users.noreply.github.com>
2024-06-16 11:22:40 +0900

    native wind

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { StatusBar } from "expo-status-bar";
+import { Redirect, router } from "expo-router";
+import { View, Text, Image, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
+
+const index = () => {
+  return (
+    <View className="text-red-300 pt-20 px-10">
+      <Text className="text-rose-500">poop</Text>
+    </View>
+  );
+};
+
+export default index;

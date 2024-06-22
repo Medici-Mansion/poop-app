b506a3146b8679deaa431b72ba2c63e450420a6a
KumJungMin <rmawjdals@naver.com>
2024-06-22 20:00:21 +0900

    feat: 반려견 이름 입력칸 추가하기

--- app/profile/create.tsx
+++ app/profile/create.tsx
@@ -1,17 +1,15 @@
 import { useRef } from 'react';
 import { router } from 'expo-router';
+import { Text, View } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { SafeAreaView } from "react-native-safe-area-context";
 import { GestureHandlerRootView, ScrollView } from "react-native-gesture-handler";
-import { Text, View } from 'react-native';
 
 import { gender } from '@/constants';
 import { useProfileStore } from '@/store/profile';
 
 import { Input } from '@/components/profile/create/input';
 import RadioGroup from '@/components/profile/create/radio-group';
-import { View } from 'react-native';
-
 import GalleryButton from '@/components/profile/create/gallery-button';
 import DateTimeSheet from '@/components/profile/create/date-time-sheet';
 import BreedSelectSheet from '@/components/profile/create/breed-select-sheet';
@@ -27,35 +25,35 @@ export default function CreateProfile() {
         {/* TODO: 스크롤 상단에 padding이 있는 현상 수정하기 */}
         <ScrollView className="w-full h-full flex-1">
           <View className="px-4 flex flex-col py-10 items-center w-full h-ful">
-            <GalleryButton 
+            <GalleryButton
               image={profileStore.profile.avatar}
-              onPress={() => router.push('select-photo')} 
-            /> 
+              onPress={() => router.push('select-photo')}
+            />
 
             <View className="w-full mt-6">
-              <Input 
+              <Input
                 label='반려견 이름'
-                placeholder='이름' 
-                onChangeText={profileStore.setName} 
+                placeholder='이름'
+                onChangeText={profileStore.setName}
                 value={profileStore.profile.name}
-              /> 
+              />
             </View>
 
             <View className="w-full mt-6">
-              <Input 
+              <Input
                 label='반려견 생년월일'
-                placeholder='생년월일' 
-                editable={false} 
+                placeholder='생년월일'
+                editable={false}
                 value={profileStore.profile.birthday}
                 onPress={() => timePicker.current?.show()}
                 />
             </View>
 
             <View className="w-full mt-6 relative">
-              <Input 
+              <Input
                 label='견종'
-                placeholder='견종 선택' 
-                editable={false} 
+                placeholder='견종 선택'
+                editable={false}
                 value={profileStore.profile.breed?.name || ''}
                 onPress={() => breedRef.current?.open()}
               />
@@ -66,8 +64,8 @@ export default function CreateProfile() {
 
             <View className="w-full mt-6">
               <Text className="text-gray-200 b-12 text-14 font-bold mb-4">성별</Text>
-              <RadioGroup 
-                options={gender} 
+              <RadioGroup
+                options={gender}
                 selectedOption={profileStore.profile.gender}
                 onSelect={profileStore.setGender}
               />

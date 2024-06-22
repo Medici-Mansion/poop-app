f63d93c6b2f5c1a87615ac4610b96ec75bd573b7
KumJungMin <rmawjdals@naver.com>
2024-06-22 23:14:51 +0900

    feat: 프로필 메인 화면  추가하기

---
+++ app/profile/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+export default function Page() {
+  return (
+    <View>
+      <Text>안녕! 나는 프로필 메인이야!</Text>
+    </View>
+  )
+}

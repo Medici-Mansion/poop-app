4abd9f5ee35f56ba34d2efed1ac2e41407d10739
KumJungMin <rmawjdals@naver.com>
2024-06-22 20:09:52 +0900

    feat: 프로필 선택 버튼 추가하기

--- app/profile/create.tsx
+++ app/profile/create.tsx
@@ -1,3 +1,4 @@
+import { router } from 'expo-router';
 import { SafeAreaView } from "react-native-safe-area-context";
 import { GestureHandlerRootView, ScrollView } from "react-native-gesture-handler";
 import { View } from 'react-native';
@@ -5,9 +6,9 @@ import { View } from 'react-native';
 import { useProfileStore } from '@/store/profile';
 
 import { Input } from '@/components/profile/create/input';
+import GalleryButton from '@/components/profile/create/gallery-button';
 
 export default function CreateProfile() {
-
   const profileStore = useProfileStore();
 
   return (
@@ -16,6 +17,11 @@ export default function CreateProfile() {
         {/* TODO: 스크롤 상단에 padding이 있는 현상 수정하기 */}
         <ScrollView className="w-full h-full flex-1">
           <View className="px-4 flex flex-col py-10 items-center w-full h-ful">
+            <GalleryButton 
+              image={profileStore.profile.avatar}
+              onPress={() => router.push('select-photo')} 
+            /> 
+
             <View className="w-full mt-6">
               <Input 
                 label='반려견 이름'
@@ -31,3 +37,4 @@ export default function CreateProfile() {
     </GestureHandlerRootView>
   );
 }
+

a397f65737ca0f2df064e7b9f6ddd56c7f70deb7
KumJungMin <rmawjdals@naver.com>
2024-06-22 20:08:44 +0900

    feat: 이미지 박스 추가하기

---
+++ components/select-photo/photo-box.tsx
@@ -0,0 +1,23 @@
+import { Pressable, Image } from 'react-native';
+
+interface ImageBoxProps {
+  item: any;
+  column: number;
+  onPress: (item: any) => void;
+}
+
+/** 
+ * PhotoBox
+ * 이미지 박스
+ * */ 
+const PhotoBox = (props: ImageBoxProps) => {
+  const { item, onPress } = props;
+  return (
+    <Pressable className="h-[120px] w-[33%] p-1" onPress={() =>  onPress(item)} >
+      <Image source={{ uri: item.uri }} style={{ height: 120 }} />
+    </Pressable>
+  );
+};
+
+
+export default PhotoBox;

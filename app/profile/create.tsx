963cd01f99d1ef95d816b94486ef9d013afdc246
KumJungMin <37934668+KumJungMin@users.noreply.github.com>
2024-07-04 08:07:19 +0900

    feat(프로필_1): 이름 입력칸 추가하기 (#1)
    * feat: 프로필 화면 스택 추가하기
    
    * feat: 테스트를 위해 메인을 profile로 변경하기(임시)
    
    * feat: 프로필 생성 스택 추가하기
    
    * feat: profile store 추가하기
    
    * feat: 프로필 화면 헤더 추가하기
    
    * feat: 반려견 이름 입력칸 추가하기
    
    * feat: onPress 영역 수정한 input 추가하기(추후 ui/input과 합치기)
    
    * fix: 작업 이외의 컴포넌트 제거하기
    
    * refactor: 영속성 보장을 위해 프로필을 AsyncStorage에 저장하기

---
+++ app/profile/create.tsx
@@ -0,0 +1,33 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { GestureHandlerRootView, ScrollView } from "react-native-gesture-handler";
+import { View } from 'react-native';
+
+import { useProfileStore } from '@/store/profile';
+
+import { Input } from '@/components/profile/create/input';
+
+export default function CreateProfile() {
+
+  const profileStore = useProfileStore();
+
+  return (
+    <GestureHandlerRootView>
+      <SafeAreaView className="w-full h-full bg-gray-600">
+        {/* TODO: 스크롤 상단에 padding이 있는 현상 수정하기 */}
+        <ScrollView className="w-full h-full flex-1">
+          <View className="px-4 flex flex-col py-10 items-center w-full h-ful">
+            <View className="w-full mt-6">
+              <Input 
+                label='반려견 이름'
+                placeholder='이름' 
+                onChangeText={profileStore.setName} 
+                value={profileStore.profile.name}
+              /> 
+            </View>
+          </View>
+
+        </ScrollView>
+      </SafeAreaView>
+    </GestureHandlerRootView>
+  );
+}
---
+++ store/profile.ts
@@ -0,0 +1,52 @@
+import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import { gender } from '@/constants';
+import type { Breed } from '@/types';
+
+type Gender = typeof gender[number]['value'];
+
+interface ProfileStore {
+  profile: Profile;
+  setName: (name: string) => void;
+  setAvatar: (avatar: string) => void;
+  setBirthday: (birthday: string) => void;
+  setGender: (gender: Gender) => void;
+  setBreed: (breed: Breed) => void;
+}
+
+interface Profile {
+  avatar: string;
+  name: string;
+  birthday: string;
+  gender: Gender;
+  breed: Breed | null;
+}
+
+const initProfile: Profile = {
+  avatar: "",
+  name: "",
+  birthday: "",
+  gender: "FEMALE",
+  breed: null,
+};
+
+
+export const useProfileStore = create<ProfileStore>()(
+  persist(
+    (set) => ({
+      profile: initProfile,
+
+      setName: (name) => set((state) => ({ profile: { ...state.profile, name } })),
+      setAvatar: (avatar) => set((state) => ({ profile: { ...state.profile, avatar } })),
+      setBirthday: (birthday) => set((state) => ({ profile: { ...state.profile, birthday } })),
+      setGender: (gender) => set((state) => ({ profile: { ...state.profile, gender } })),
+      setBreed: (breed) => set((state) => ({ profile: { ...state.profile, breed } })),
+    }),
+    {
+      name: 'profile-storage',
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);

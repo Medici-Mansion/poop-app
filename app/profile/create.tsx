33514cf4d2eb017d93221e57a4b858a899dcac12
KumJungMin <37934668+KumJungMin@users.noreply.github.com>
2024-07-04 08:09:39 +0900

    feat(프로필_2): 사진 선택 화면 추가하기 (#2)
    * feat: 프로필 화면 스택 추가하기
    
    * feat: 테스트를 위해 메인을 profile로 변경하기(임시)
    
    * feat: 프로필 생성 스택 추가하기
    
    * feat: profile store 추가하기
    
    * feat: 프로필 화면 헤더 추가하기
    
    * feat: 반려견 이름 입력칸 추가하기
    
    * feat: onPress 영역 수정한 input 추가하기(추후 ui/input과 합치기)
    
    * fix: 작업 이외의 컴포넌트 제거하기
    
    * feat: 사진 선택 화면 추가하기
    
    * feat: 사진 로드 추가하기
    
    * feat: 사진 선택 화면 메인 헤더 추가하기
    
    * feat: 이미지 박스 추가하기
    
    * feat: 프로필 선택 버튼 추가하기
    
    * style: image의 너비, 높이를 부모 기준으로 변경하기

--- app/profile/create.tsx
+++ app/profile/create.tsx
@@ -1,3 +1,4 @@
+import { router } from 'expo-router';
 import { SafeAreaView } from "react-native-safe-area-context";
 import { GestureHandlerRootView, ScrollView } from "react-native-gesture-handler";
 import { View } from 'react-native';
@@ -5,6 +6,7 @@ import { View } from 'react-native';
 import { useProfileStore } from '@/store/profile';
 
 import { Input } from '@/components/profile/create/input';
+import GalleryButton from '@/components/profile/create/gallery-button';
 
 export default function CreateProfile() {
 
@@ -16,6 +18,10 @@ export default function CreateProfile() {
         {/* TODO: 스크롤 상단에 padding이 있는 현상 수정하기 */}
         <ScrollView className="w-full h-full flex-1">
           <View className="px-4 flex flex-col py-10 items-center w-full h-ful">
+            <GalleryButton 
+              image={profileStore.profile.avatar}
+              onPress={() => router.push('select-photo')} 
+            /> 
             <View className="w-full mt-6">
               <Input 
                 label='반려견 이름'
@@ -31,3 +37,4 @@ export default function CreateProfile() {
     </GestureHandlerRootView>
   );
 }
+

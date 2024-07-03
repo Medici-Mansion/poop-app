c2dc78d6ad97b4c063fab52cfeb2904e3b43580b
KumJungMin <37934668+KumJungMin@users.noreply.github.com>
2024-07-04 08:29:57 +0900

    chore: 사용하지 않는 props 제거하기

--- components/profile/create/name-input.tsx
+++ components/profile/create/name-input.tsx
@@ -5,12 +5,12 @@ import { Input } from '@/components/profile/create/input';
 
 const MIN_LENGTH = 2;
 
-/** 
+/**
  * NameInput
  * 반려견 이름 입력 컴포넌트
  * 이름이 2자 이상인지 유효성 검사
- * */ 
-const NameInput = forwardRef((props, ref: React.Ref<any>) => {
+ * */
+const NameInput = forwardRef((ref: React.Ref<any>) => {
   const [error, setError] = useState<null | string>(null);
   const profileStore = useProfileStore();
 
@@ -21,7 +21,7 @@ const NameInput = forwardRef((props, ref: React.Ref<any>) => {
       setError(null);
     }
     router.setParams({ event: null });
-    
+
     return !error;
   };
 
@@ -35,13 +35,13 @@ const NameInput = forwardRef((props, ref: React.Ref<any>) => {
   };
 
   return (
-    <Input 
+    <Input
       label='반려견 이름'
-      placeholder='이름' 
+      placeholder='이름'
       error={error || ''}
-      onChangeText={handleChangeName} 
+      onChangeText={handleChangeName}
       value={profileStore.profile.name}
-    /> 
+    />
   );
 });
 

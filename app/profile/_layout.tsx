763daef250e808d99b80b1e5e469b1dd68eb99ce
KumJungMin <rmawjdals@naver.com>
2024-07-14 21:47:22 +0900

    feat: 내 프로필 화면 추가하기

--- app/profile/_layout.tsx
+++ app/profile/_layout.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import Header from "@/components/profile/create/header";
+import CreateHeader from "@/components/profile/create/header";
+import MyProfileHeader from "@/components/profile/my-profile/header";
+
 const ProfileLayout = () => {
   return (
     <>
@@ -9,8 +11,16 @@ const ProfileLayout = () => {
         <Stack.Screen name="create" 
           options={{
             title: "",
-            headerLeft: () => <Header.LeftButton />,
-            headerRight: () => <Header.RightButton />,
+            headerLeft: () => <CreateHeader.LeftButton />,
+            headerRight: () => <CreateHeader.RightButton />,
+            headerStyle: { backgroundColor: "#161622" },
+          }} 
+        />
+        <Stack.Screen name="my-profile" 
+          options={{
+            title: "",
+            headerLeft: () => <MyProfileHeader.LeftButton />,
+            headerRight: () => <MyProfileHeader.RightButton />,
             headerStyle: { backgroundColor: "#161622" },
           }} 
         />

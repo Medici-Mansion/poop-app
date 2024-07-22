b75740b7192008d2635ed3e97f8119b48f7e7715
KumJungMin <rmawjdals@naver.com>
2024-07-22 21:34:48 +0900

    refactor: 컴포넌트 export 방식 수정하기

--- components/my-profile/main/index.ts
+++ components/my-profile/main/index.ts
@@ -1,2 +1,4 @@
 export { default as ControlButtons } from "./control-buttons";
 export { default as Header } from "./header";
+export { default as MenuTabs, Tab as MenuTabType } from "./menu-tabs";
+export { default as Content } from "./content";

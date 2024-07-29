6e288e6eb4cf1a83c74e0bfc1900ae5ac2ce4807
Raymond <akdfhr2@gmail.com>
2024-07-30 02:10:15 +0900

    feat: 아이콘 추가하기

--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -2,3 +2,22 @@ export { default as InputDeleteButton } from "./input-delete-button.svg";
 export { default as InputCheckbutton } from "./input-check-button.svg";
 export { default as InputEyeOn } from "./input-eye-on.svg";
 export { default as InputEyeOff } from "./input-eye-off.svg";
+export { default as CheckInactive } from "./check_inactive.svg";
+export { default as CheckActive } from "./check_active.svg";
+
+export { default as ArrowInactive } from "./arrow_inactive.svg";
+export { default as ArrowActive } from "./arrow_active.svg";
+
+export { default as Challenge } from "./challenge.svg";
+export { default as Write } from "./write.svg";
+
+export { default as ChavronLeft } from "./chevron_left.svg";
+
+export { default as ArrowSmall } from "./arrow_small.svg";
+export { default as ArrowLeft } from "./arrow_left.svg";
+
+// Bottom Tab Icons
+export { default as Community } from "./community_bottomtab.svg";
+export { default as Profile } from "./profile_bottomtab.svg";
+export { default as Plus } from "./plus_bottomtab.svg";
+export { default as PlusActive } from "./plus-active_bottomtab.svg";
